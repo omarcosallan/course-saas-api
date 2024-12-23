07bdd463ca8a3756240bfa2776c93a3f2efe5bff
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-23 09:13:57 -0300

    feta: initial commit

---
+++ src/main/java/com/omarcosallan/saas_api/SaasApiApplication.java
@@ -0,0 +1,13 @@
+package com.omarcosallan.saas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaasApiApplication.class, args);
+	}
+
+}
